91f9b41203d1f650f2c35c777d08117371cc3314
Zelto Lira <you@example.com>
2023-03-21 20:48:57 -0300

    inicia projeto

---
+++ src/main/java/br/com/lembrete/conjugelembrete/ConjugeLembreteApplication.java
@@ -0,0 +1,13 @@
+package br.com.lembrete.conjugelembrete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConjugeLembreteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConjugeLembreteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/lembrete/conjugelembrete/ConjugeLembreteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lembrete.conjugelembrete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConjugeLembreteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
