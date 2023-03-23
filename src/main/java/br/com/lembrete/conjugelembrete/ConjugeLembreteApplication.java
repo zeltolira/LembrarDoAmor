0a5906136cc0ed24e19bbcd52a61747be5d26a82
Zelto Lira <you@example.com>
2023-03-23 18:58:52 -0300

    adiciona banco de dados

--- src/main/java/br/com/lembrete/conjugelembrete/ConjugeLembreteApplication.java
+++ src/main/java/br/com/lembrete/conjugelembrete/ConjugeLembreteApplication.java
@@ -2,10 +2,19 @@ package br.com.lembrete.conjugelembrete;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ConjugeLembreteApplication {
 
+	@GetMapping("/teste")
+	public String getHomeTest() {
+		return "Lembrando do meu amor - API Home";
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ConjugeLembreteApplication.class, args);
 	}
