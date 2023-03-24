37192f24ba464107abc73e04b34b6076845a35b6
Zelto Lira <you@example.com>
2023-03-24 06:07:40 -0300

    implementa request e response

--- src/main/java/br/com/lembrete/conjugelembrete/application/api/ConjugeRequest.java
+++ src/main/java/br/com/lembrete/conjugelembrete/application/api/ConjugeRequest.java
@@ -1,5 +1,23 @@
 package br.com.lembrete.conjugelembrete.application.api;
 
+import java.time.LocalDate;
+import java.util.UUID;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import br.com.lembrete.conjugelembrete.domain.Sexo;
+import lombok.Value;
+
+@Value
 public class ConjugeRequest {
 
+	@NotBlank
+	private String nome;
+	@NotNull
+	private Sexo sexo;
+	@NotBlank
+	private String celular;
+	@NotNull
+	private LocalDate dataNascimento;
 }
--- src/main/java/br/com/lembrete/conjugelembrete/application/api/ConjugeResponse.java
+++ src/main/java/br/com/lembrete/conjugelembrete/application/api/ConjugeResponse.java
@@ -1,5 +1,11 @@
 package br.com.lembrete.conjugelembrete.application.api;
 
+import java.util.UUID;
+
+import lombok.Value;
+
+@Value
 public class ConjugeResponse {
 
+	private UUID idConjuge;
 }
